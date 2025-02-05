5b93e0a5970f79c662bc65d1e092d50b817c738b
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-02-05 17:53:00 +0530

    add forgot password functionality

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,24 +1,36 @@
-const express = require('express')
+const express = require("express");
 
-const {getusers, createUser, userSignin, verifyEmail, resendVerificationEmail, GoogleSignIn} = require("../Controllers/userController")
+const {
+  getusers,
+  createUser,
+  userSignin,
+  verifyEmail,
+  resendVerificationEmail,
+  GoogleSignIn,
+  forgotPassword,
+  resetPassword,
+} = require("../Controllers/userController");
 
-const router = express.Router()
+const router = express.Router();
 
-// get Users Table 
-router.get('/', getusers)
+// get Users Table
+router.get("/", getusers);
 //register user
-router.post('/register', createUser)
+router.post("/register", createUser);
 
 // Login user
-router.post('/signin', userSignin)
+router.post("/signin", userSignin);
 
 //login using signin button
 
-router.post('/auth/google', GoogleSignIn)
+router.post("/auth/google", GoogleSignIn);
 
-router.get('/verifyEmail', verifyEmail)
+router.get("/verifyEmail", verifyEmail);
 
-router.post('/reVerifyMail', resendVerificationEmail)
+router.post("/reVerifyMail", resendVerificationEmail);
 
+//reset password routes
+router.post("/forget-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
-module.exports = router
+module.exports = router;
